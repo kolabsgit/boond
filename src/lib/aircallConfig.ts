d91e35bdabed82ec6e19af1a462f4cc85b397265
kolabsgit <107631423+kolabsgit@users.noreply.github.com>
2022-08-01 18:59:25 +0200

    add files

---
+++ src/lib/aircallConfig.ts
@@ -0,0 +1,9 @@
+
+// export const AIRCALL_API_ID = process.env.AIRCALL_API_ID || "6834bfab84bf5f878c71bb274b49f062";
+// export const AIRCALL_API_TOKEN = process.env.AIRCALL_API_TOKEN || "ae5ed7cb661f2ee07ca15573e0738363";
+// export const AIRCALL_AUTH_TOKEN = process.env.AIRCALL_AUTH_TOKEN || "Basic NjgzNGJmYWI4NGJmNWY4NzhjNzFiYjI3NGI0OWYwNjI6YWU1ZWQ3Y2I2NjFmMmVlMDdjYTE1NTczZTA3MzgzNjM="
+// //================================================================
+
+export const AIRCALL_API_ID = "031e2b63f7c4de70381c9dd4b8bf4dbf";
+export const AIRCALL_API_TOKEN = "4bc00d741998c4239e9b41c92df1f42a";
+export const AIRCALL_AUTH_TOKEN = "Basic MDMxZTJiNjNmN2M0ZGU3MDM4MWM5ZGQ0YjhiZjRkYmY6NGJjMDBkNzQxOTk4YzQyMzllOWI0MWM5MmRmMWY0MmE="
